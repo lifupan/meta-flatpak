8669efff3970110630b2e097d7c3df8525e96b69
Fupan Li <fupan.li@windriver.com>
2018-04-10 23:10:55 -0700

    wr-core: fix the issue of license
    Signed-off-by: Fupan Li <fupan.li@windriver.com>

--- recipes-sota/ostree-initrd/files/init.sh
+++ recipes-sota/ostree-initrd/files/init.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-#
-# Copyright (C) 2018 Wind River Systems Inc.
-#
+#/*
+#*init.sh , a script to init the ostree system in initramfs
+#* 
+#* Copyright (c) 2018 Wind River Systems, Inc.
+#* 
+#* This program is free software; you can redistribute it and/or modify
+#* it under the terms of the GNU General Public License version 2 as
+#* published by the Free Software Foundation.
+#* 
+#* This program is distributed in the hope that it will be useful,
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#* See the GNU General Public License for more details.
+#* 
+#* You should have received a copy of the GNU General Public License
+#* along with this program; if not, write to the Free Software
+#* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#* 
+#*/ 
 
 set -eu
 # -------------------------------------------
@@ -24,7 +40,7 @@ fluxdataexpander_run() {
     parted -s /dev/$datadev -- resizepart $datadevnum -1s
     log_info "fluxdataexpander: Finished expanding data partition."
 
-    partprobe /dev/$datadev
+    partprobe
     sync
 }
 
